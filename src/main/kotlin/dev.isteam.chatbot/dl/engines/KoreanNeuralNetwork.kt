adbb09c8410a8f8441371f91d883005ffa8a1852
Singlerr <singlerr@soongsil.ac.kr>
2022-04-05 13:41:02 +0900

    Commit - LSTM Remastered

--- src/main/kotlin/dev.isteam.chatbot/dl/engines/KoreanNeuralNetwork.kt
+++ src/main/kotlin/dev.isteam.chatbot/dl/engines/KoreanNeuralNetwork.kt
@@ -171,8 +171,9 @@ object KoreanNeuralNetwork {
 
     fun buildNeuralNetworkLSTM(inputSize: Int, outputSize: Int): MultiLayerNetwork{
         var conf = NeuralNetConfiguration.Builder()
-            .updater(RmsProp(1e-2))
+            .updater(Nesterovs(1e-1))
             .activation(Activation.TANH)
+            .weightInit(WeightInit.XAVIER)
             .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
             .seed(13373)
             .list()
