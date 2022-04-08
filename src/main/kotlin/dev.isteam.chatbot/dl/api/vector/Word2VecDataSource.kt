d10ab34515b80f681e7cd9e8dd4003429ae82d01
Singlerr <singlerr@soongsil.ac.kr>
2022-04-08 17:15:54 +0900

    Commit - LSTM Remastered

--- src/main/kotlin/dev.isteam.chatbot/dl/api/vector/Word2VecDataSource.kt
+++ src/main/kotlin/dev.isteam.chatbot/dl/api/vector/Word2VecDataSource.kt
@@ -64,7 +64,7 @@ class Word2VecDataSource(
         var features = Nd4j.zeros(num,maxLen,word2Vec.layerSize)
         var labels = Nd4j.zeros(num,maxLen,word2Vec.layerSize)
         for(i in 0 until batchSize){
-            if(! hasNext()) throw NoSuchElementException()
+            if(! hasNext()) break
             var sentenceVec = Nd4j.create(maxLen,word2Vec.layerSize)
 
             var tokenizer = koreanTokenizerFactory.create(packedRawDataSet.x[packedRawDataSet.x.size - sentencesLeft])
