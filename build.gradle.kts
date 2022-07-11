e4e04f52c8ae8f9b1f1bb1ae145f2f867c0ad498
Singlerr <singlerr@soongsil.ac.kr>
2022-07-11 23:33:29 +0900

    Commit - Fixed ClassNotFoundException when startup

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,52 +20,62 @@ repositories {
     }
 }
 
-val externalLib: Configuration by configurations.creating
-configurations.implementation.get().extendsFrom(externalLib)
 dependencies {
     implementation(kotlin("stdlib"))
-    externalLib(group = "me.tongfei", name = "progressbar", version = "0.9.3")
-    externalLib(group = "com.github.shin285", name = "KOMORAN", version = "3.3.4")
-    externalLib(group = "org.json", name = "json", version = "20211205")
+    implementation(group = "me.tongfei", name = "progressbar", version = "0.9.3")
+    implementation(group = "com.github.shin285", name = "KOMORAN", version = "3.3.4")
+    implementation(group = "org.json", name = "json", version = "20211205")
     // implementation(group = "org.nd4j", name = "nd4j-native", version = nd4jVersion)
     // implementation(group = "org.nd4j", name = "nd4j-native", version = nd4jVersion, classifier = "linux-x86_64-avx2")
     //implementation(group = "org.nd4j", name = "nd4j-native-platform", version = nd4jVersion)
     //  implementation(group = "org.nd4j", name = "nd4j-cuda-10.0", version = nd4jVersion)
     //  implementation(group = "org.nd4j", name = "nd4j-cuda-10.0", version = nd4jVersion, classifier="windows-x86_64")
-    externalLib(group = "org.nd4j", name = "nd4j-cuda-11.2", version = nd4jVersion)
-    externalLib("org.deeplearning4j:deeplearning4j-modelimport:1.0.0-M2")
-    externalLib(group = "org.deeplearning4j", name="deeplearning4j-ui", version= dl4jVersion)
-    externalLib(group = "org.deeplearning4j", name = "deeplearning4j-cuda-11.2", version = dl4jVersion)
-    externalLib(group = "org.deeplearning4j", name = "deeplearning4j-core", version = dl4jVersion)
-    externalLib(group = "org.deeplearning4j", name = "deeplearning4j-nlp", version = dl4jVersion)
-    externalLib(group = "org.junit.jupiter", name = "junit-jupiter", version = "5.7.0")
-    externalLib(group = "ch.qos.logback", name = "logback-classic", version = "1.2.6")
+    implementation(group = "org.nd4j", name = "nd4j-cuda-11.2", version = nd4jVersion)
+    implementation("org.deeplearning4j:deeplearning4j-modelimport:1.0.0-M2")
+    implementation(group = "org.deeplearning4j", name = "deeplearning4j-ui", version = dl4jVersion)
+    implementation(group = "org.deeplearning4j", name = "deeplearning4j-cuda-11.2", version = dl4jVersion)
+    implementation(group = "org.deeplearning4j", name = "deeplearning4j-core", version = dl4jVersion)
+    implementation(group = "org.deeplearning4j", name = "deeplearning4j-nlp", version = dl4jVersion)
+    implementation(group = "org.junit.jupiter", name = "junit-jupiter", version = "5.7.0")
+    implementation(group = "ch.qos.logback", name = "logback-classic", version = "1.2.6")
 }
-project.setProperty("mainClassName","dev.isteam.chatbot.MainKt")
+project.setProperty("mainClassName", "dev.isteam.chatbot.MainKt")
 
 tasks.withType<ShadowJar>{
     isZip64 = true
     archiveClassifier.set("")
 }
-
-tasks.create("copyAllExternalLibs"){
+tasks.build{
+    dependsOn("shadowJar")
+}
+/*
+tasks.withType<Jar> {
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    isZip64 = true
+    manifest {
+        attributes("Main-Class" to "dev.isteam.chatbot.MainKt")
+    }
+    from(configurations.compileClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+}
+ */
+/*
+tasks.create("copyAllExternalLibs") {
     externalLib.forEach { lib ->
-        val file = File(externalLibs,lib.name)
-        if(! file.exists()){
-            lib.copyTo(file,overwrite = true)
+        val file = File(externalLibs, lib.name)
+        if (!file.exists()) {
+            lib.copyTo(file, overwrite = true)
             println("Copying ${lib.name} to $externalLibs")
         }
 
     }
 }
 
+ */
+
 group = "dev.isteam"
 version = "1.0-SNAPSHOT"
 description = rootProject.name
 java.sourceCompatibility = JavaVersion.VERSION_11
-tasks.build{
-    dependsOn("shadowJar")
-}
 /*
 application{
     mainClass.set("dev.isteam.chatbot.Main")
